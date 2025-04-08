3cf3f700953072a32b09238562da38802f769fad
Saileonot <tonoelias@gmail.com>
2025-04-08 10:56:44 +0200

    Update script.js acortar direccion remiitente
    Acortar dirección remitente

--- script.js
+++ script.js
@@ -875,7 +875,7 @@ function generatePDF() {
             'C/Extremadura nº 2, buzón 30',
             '41909 Salteras (Sevilla). SPAIN',
             'Phone: +34 955 110 357',
-            'Contact Details: Alberto Sandino - (+34) 627 068 429 - alberto@xtraice.com',
+            'Alberto Sandino - 627 068 429 - alberto@xtraice.com',
             ' '
         ], 14, 20);
 
@@ -1093,7 +1093,7 @@ async function exportToExcel() {
         'C/Extremadura nº 2, buzón 30',
         '41909 Salteras (Sevilla). SPAIN',
         'Phone: +34 955 110 357',
-        'Contact Details: Alberto Sandino - (+34) 627 068 429 - alberto@xtraice.com'
+        'Alberto Sandino - 627 068 429 - alberto@xtraice.com'
     ];
     remitente.forEach((line, i) => {
         sheet.getCell(`A${i + 3}`).value = line;
